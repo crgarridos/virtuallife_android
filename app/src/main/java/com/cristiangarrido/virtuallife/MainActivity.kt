c9b6ac30f92bc32c827e572d12c5c01e9b4b4f87
Cristian Garrido <crgarrido.s@gmail.com>
2017-02-05 22:50:05 +0100

    Integrating Firebase

--- app/src/main/java/com/cristiangarrido/virtuallife/MainActivity.kt
+++ app/src/main/java/com/cristiangarrido/virtuallife/MainActivity.kt
@@ -3,6 +3,10 @@ package com.cristiangarrido.virtuallife
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_main.*
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -10,5 +14,11 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         mainText.text = "leita"
+
+        // Write a message to the database
+        val database = FirebaseDatabase.getInstance()
+        val myRef = database.getReference("message")
+
+        myRef.setValue("Hello, World!")
     }
 }
